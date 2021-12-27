7485ff5474d2aa99366b839841ea5dcf4c5fe2c9
Liban-Madar <liban.madar@revaure.net>
2021-12-27 10:30:08 -0600

    first commit with new project0

---
+++ Project0/Project0/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Customer
+{
+	public customer()
+	{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int? DefaultLocationId { get; set; }
+    public int Id { get; set; }
+    
+    
+}
+
+}
---
+++ Project0/Project0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+namespace Project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                 
+        
+            Console.WriteLine("Welcome to Store!");
+            Console.WriteLine("Please Select From the Following Options: ");
+            Console.WriteLine("(1) Add Customer");
+            Console.WriteLine("(2) View All Customers");
+            Console.WriteLine("(3) Change Customer");
+            Console.WriteLine("(4) Add Location");
+            Console.WriteLine("(5) View All Locations");
+            
+        
+
+
+
+            
+        }
+    }
+
